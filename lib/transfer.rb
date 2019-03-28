8463b9deb72adb95d0ea35dd268b72bcabd774dc
Paige Wiener <snowplushie@yahoo.com>
2019-03-28 23:52:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
     if valid? && sender.balance > amount && self.status = "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
+      self.status = "complete"
     else
       reject_transfer
     end
